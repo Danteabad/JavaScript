739112984b16f61b714cc028b475ee2667001997
Danteabad <94439928+Danteabad@users.noreply.github.com>
2021-11-22 20:55:16 +0100

    Add files via upload

---
+++ EJ_1.2.js
@@ -0,0 +1,43 @@
+//EX6
+//let num1 = 1234;
+//let num2 = 5678;
+//console.log((num1 + num2).toFixed(1))
+//console.log((num1 - num2).toFixed(1))
+//console.log((num1 / num2).toFixed(1))
+//console.log((num1 * num2).toFixed(1))
+
+//EX7
+//let catets = 4;
+//console.log((4*4) + (4*4))
+
+//EX8
+//var num1 = 45;
+//var string = 'Dante'
+//console.log(num1/string)
+
+//EX9
+//var nom = 'Dante Abad Jiménez'
+//var cog = nom.slice(5,10)
+//console.log(cog)
+
+//EX10
+//var nom = 'Dante Abad Jiménez'
+//var esborra = nom.replace('A', '')
+//console.log(esborra)
+
+//EX11
+//var var1 = undefined
+//var var2 = null
+//console.log(var1 + var2)
+
+//EX12
+//console.log(String(true))
+//console.log(String + true)
+
+//EX13
+//console.log(Boolean('Hey com anem'))
+//console.log(true || 'hola bon dia')
+
+//EX14
+//console.log(Number(true))
+//console.log(45 != true)
---
+++ EJ_1.js
@@ -0,0 +1,25 @@
+//EX1
+//console.log("Dante");
+//console.warn("Abad Jiménez");
+//console.error("8/10/2003");
+
+//EX2
+//var variable1 = "Hola"
+//let variable2 = "Hola que tal"
+//const variable3 = "Adeu"
+//console.log(variable1)
+//console.log(variable2)
+//console.log(variable3)
+
+//EX3
+//var variable = "Hola"
+//console.log(variable)
+
+//EX4
+//var variable = 23
+//console.log(variable)
+
+//EX5
+//const CONSTANT = 'Dante'
+//console.log(CONSTANT)
+//CONSTANT = 'Pablo'
